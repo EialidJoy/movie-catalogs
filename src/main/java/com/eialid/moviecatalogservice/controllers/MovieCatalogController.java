c968160087084a510df529be000674fde1ee94b3
Eialid Ahmed Joy <33479362+EialidJoy@users.noreply.github.com>
2022-04-16 23:50:59 +0600

    eureka client registered

--- src/main/java/com/eialid/moviecatalogservice/controllers/MovieCatalogController.java
+++ src/main/java/com/eialid/moviecatalogservice/controllers/MovieCatalogController.java
@@ -32,12 +32,14 @@ public class MovieCatalogController {
 
         return ratings.getUserRatings().stream().map(rating -> {
            Movie movie=restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-            /* Movie movie = webClientBuilder.build() // webClient build method is implemented to get webCLient object
+
+           /* Movie movie = webClientBuilder.build() // webClient build method is implemented to get webCLient object
                     .get()                         // mentioning whether it is get or post
                     .uri("http://localhost:8082/movies/" + rating.getMovieId())  // passing the uri from which it connects
                     .retrieve()                     // retrieve all the data that it gets
                     .bodyToMono(Movie.class)        // and converts it to Movie class type of object, bodyToMono is basically a emptyContainer u can say, it is type of promise where it is written what must be done when it gets the data. I am saying inside of this function, please convert the data to Movie object for me.. it is must for Webclient technique since we are doing asynchronous programming here.
                     .block();                       // It basically makes object ready and let know to me that it is ready now.  */
+
             return new MovieCatalog(movie.getName(), "A spiderman series of movies", rating.getRatings());
         }).collect(Collectors.toList());
     }
